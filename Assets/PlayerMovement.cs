13506bdb687fc6a875c890d37b0cddc2a3dca1b0
e2ik <erik.jensen.lin@gmail.com>
2025-07-10 21:47:34 +1000

    cinemachine freelook

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour {
     public float maxSpeed = 10f;
     public float jumpForce = 10f;
     private Vector2 _moveDirection;
+    public bool followCameraRotation = false;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -16,8 +17,34 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        MovementLogic();
+    }
+
+    void OnMove(InputValue value) {
+        _moveDirection = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value) {
+        if (value.isPressed && collisionCheck.IsGrounded) {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
+    }
+
+    void MovementLogic() {
+        Transform cam = Camera.main.transform;
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
         Vector3 inputDir = new Vector3(_moveDirection.x, 0, _moveDirection.y).normalized;
         float horizontalSpeed = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z).magnitude;
+
+        if (followCameraRotation) {
+            inputDir = (camForward * _moveDirection.y + camRight * _moveDirection.x).normalized;
+        }
 
         if (horizontalSpeed < maxSpeed) {
             if (collisionCheck.TryGetGroundNormal(out Vector3 normal)) {
@@ -28,15 +55,10 @@ public class PlayerMovement : MonoBehaviour {
                 rb.AddForce(inputDir * acceleration, ForceMode.Acceleration);
             }
         }
-    }
 
-    void OnMove(InputValue value) {
-        _moveDirection = value.Get<Vector2>();
-    }
-
-    void OnJump(InputValue value) {
-        if (value.isPressed && collisionCheck.IsGrounded) {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        if (inputDir != Vector3.zero) {
+            Quaternion targetRotation = Quaternion.LookRotation(inputDir, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.fixedDeltaTime);
         }
     }
 }
